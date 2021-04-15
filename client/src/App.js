4211baf816c8d4be7489afb6b2fad633cf1f85ca
Chiranjibichapagain <chiranjibichapagain@gmail.com>
2021-04-15 23:14:49 +0300

    [Chore] clearing items

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,9 @@
 import React from 'react';
 
-import Button from './components/Button';
 import './app.scss';
 
 function App() {
-  const submit = () => {
-    console.log('pressed');
-  };
-  return (
-    <div className="app">
-      <h1>Welcome to the app</h1>
-      <Button modifier="active" text="Register" handleClick={submit} />
-      <Button
-        modifier="disabled"
-        disabled={true}
-        text="Register"
-        handleClick={submit}
-      />
-    </div>
-  );
+  return <div className="app">Helloo</div>;
 }
 
 export default App;
